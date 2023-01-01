255a66ba2ef1e8185e1c463546661b6e38f4bb1e
Emery-S <112206035+Emery-S@users.noreply.github.com>
2023-01-01 13:01:43 -0500

    schema

---
+++ Saved_Code/schema.sql
@@ -0,0 +1,57 @@
+-- creating table for PH-EmployeeDB
+Create Table departments(
+	dept_no VARCHAR(4) Not NULL,
+	dept_name VARCHAR(40) Not NULL,
+	PRIMARY KEY (dept_no),
+	UNIQUE (dept_name)
+);
+
+Create Table employees(
+	emp_no INT Not NULL,
+	birth_date DATE Not NULL,
+	first_name VARCHAR Not NULL,
+	last_name VARCHAR Not NULL,
+	genger VARCHAR Not NULL,
+	hire_date DATE Not NULL,
+	PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE salaries (
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE dept_emp (
+  emp_no INT NOT NULL,
+  dept_no VARCHAR NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+  PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE titles (
+  emp_no INT NOT NULL,
+  title VARCHAR NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
+
+SELECT*FROM departments;
