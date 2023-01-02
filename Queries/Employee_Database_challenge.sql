238097e586a21253aaafff31a371a9267d24ba5b
Emery-S <112206035+Emery-S@users.noreply.github.com>
2023-01-01 22:07:08 -0500

    Deliverable 2

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -27,3 +27,19 @@ SELECT COUNT(ut.emp_no),
 FROM unique_titles as ut
 GROUP BY title 
 ORDER BY COUNT(title) DESC;
+
+SELECT DISTINCT ON(e.emp_no) e.emp_no, 
+    e.first_name, 
+    e.last_name, 
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    ti.title
+INTO mentorship_eligibilty
+FROM employees as e
+Left outer Join dept_emp as de
+	ON (e.emp_no = de.emp_no)
+Left outer Join titles as ti
+	ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
